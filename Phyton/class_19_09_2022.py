cfed1aa99773cb5c354af490cb798c9419ef0016
Valmir Barros Personal <valmirpsn@gamil.com.br>
2022-09-19 20:52:12 -0400

    Assgiment 02 09/19/2022

---
+++ Phyton/class_19_09_2022.py
@@ -0,0 +1,29 @@
+# in python is for  single line comment
+# """used for comments in multiple lines"""
+
+#-----------------------------------VARIABLES-------------------
+#  variable in phython are dynamic>>>>>>>> no declarentions
+# primitive variables
+a=3 #integer
+b=1.45 # float 
+c=True # boolean
+
+d='name' # strings which colection of characters
+e= "name"
+
+# rules of naming variables
+# 0 name  varialble properly (meanigfull)
+# 1 you canot start with number
+# 2 there is no space (_ or camial case)
+# 3 no special caraters
+
+# ---------------------------------output----------------------
+
+name = 'valmir'
+var = 28
+# how  to costumize 
+print("""my name  is VAlmr an I have "shit" """)
+print("my name  is", name, "I have", var )#1 most used
+print("my name  is %s an I have %d " %(name,var))#2
+print("my name  is {} an I have {} ".format(name, var))#3
+print(f"my name  is {name} an I have {var} ") #4 mos common
