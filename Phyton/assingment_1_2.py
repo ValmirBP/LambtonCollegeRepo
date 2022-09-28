63bc16420c891fd14ebe3c4d46fcbb0c1141b020
Valmir Barros Personal <valmirpsn@gamil.com.br>
2022-09-27 20:27:58 -0400

    started the assignnment 01 and 02 [visual of main menu coded]

--- Phyton/assingment_1_2.py
+++ Phyton/assingment_1_2.py
@@ -1,12 +1,14 @@
-#  assignment 1 and 2 
-#  Student Name: Valmir de Barros Pedro
-# Student Number C0868075
-# Course: CSD 1233 Python Programming
+from emoji import emojize
 
 print (" \n Based on the following  game we designed in the class: \n")
-print ("create menu-driven programming that the user can select what kind of game they want to play\n")
+print ("create menu-driven programming that the user can select what kind of game they want to play\n".upper())
 
 def game_menu ():
-    print("\033[34m Main Menu \033[m".center(50, "#"))
+    print(" MAIN MENU".center(50, emojize(":video_game:")))
+    print('1 - SINGLE PLAYER')
+    print('2 - MULTIPLAYER')
+    print('3 - play the game two player (alternative): ')
+    print('4 - exit')
+    return input('Select the menu : (1,2,3, 4) : ')
 
-game_menu()
+
