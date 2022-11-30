93b1722aece30afaeb05cf948d05e0f07c337371
Valmir Barros Personal <valmirpsn@gamil.com.br>
2022-11-29 21:41:33 -0500

    add queries

--- Phyton/PROJECT SQL STATMENTS.sql
+++ Phyton/PROJECT SQL STATMENTS.sql
@@ -1,62 +1,219 @@
+-- products table
+
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES(1,'ecobee Smart Thermostat Premium', 'ecobee Smart Thermostat Premium ', 'smart');
+VALUES(1,'ecobee Smart Thermostat Premium', 'ecobee Smart Thermostat Premium ', 'smart'); climate
 
 INSERT INTO `term_project`.`product`(`product_id`, `title`, `description`, `type`) 
-VALUES(2,'Smart WiFi Light Bulb LED RGB Color Changing', 'Smart WiFi Light Bulb LED RGB Color Changing Compatible with Amazon Alexa and Google Home Assistant No Hub Required A19 E26 Multicolor LUMIMAN 2 Pack', 'Eletronics');
+VALUES(2,'Smart WiFi Light Bulb LED RGB Color Changing', 'Smart WiFi Light Bulb LED RGB Color Changing Compatible with Amazon Alexa and Google Home Assistant No Hub Required A19 E26 Multicolor LUMIMAN 2 Pack', 'Eletronics'); home aply
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (3,'Emerson Sensi Wi-Fi Thermostat', 'Emerson Sensi Wi-Fi Thermostat for Smart Home, ST55, DIY Version', 'smart');
+VALUES (3,'Emerson Sensi Wi-Fi Thermostat', 'Emerson Sensi Wi-Fi Thermostat for Smart Home, ST55, DIY Version', 'smart'); climate
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (4,'Senville 12000 BTU', 'Senville SENL-12CD 12000 BTU Ductless Mini Split Air Conditioner and Heat Pump, White', 'climatization');
+VALUES (4,'Senville 12000 BTU', 'Senville SENL-12CD 12000 BTU Ductless Mini Split Air Conditioner and Heat Pump, White', 'climatization'); climate
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (5,'OHMAX Mini Smart Plug', 'OHMAX Mini Smart Plug, Smart Home 2.4G Wi-Fi Plug Work with Alexa and Google Home, App Remote & Voice Control Smart Outlet Plug with Timer Surge Protector,4 Pack', 'Eletrical');
+VALUES (5,'OHMAX Mini Smart Plug', 'OHMAX Mini Smart Plug, Smart Home 2.4G Wi-Fi Plug Work with Alexa and Google Home, App Remote & Voice Control Smart Outlet Plug with Timer Surge Protector,4 Pack', 'Eletrical'); Eletrical
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (6,'Smart LED Light Strip', 'Smart LED Light Strip, 9.6ft TV Backlight WiFi Works with Alexa & Google Assistant, App Control, Music Sync 16 Million RGB Color Changing Dimmable, Home Light Decor, WiFi 2.4Ghz', 'Decoration');
+VALUES (6,'Smart LED Light Strip', 'Smart LED Light Strip, 9.6ft TV Backlight WiFi Works with Alexa & Google Assistant, App Control, Music Sync 16 Million RGB Color Changing Dimmable, Home Light Decor, WiFi 2.4Ghz', 'Decoration'); decoration
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (7,'Revolution InstaGLO R180 Toaster', 'Revolution InstaGLO R180 Toaster. 2-Slice, high-end stainless steel design. Features touchscreen with high-speed smart settings for perfectly toasted bagels, English muffins, toast, Pop-Tarts and waffles. Fresh, frozen, or reheat with 7 toastiness levels.', 'Kitchen');
+VALUES (7,'Revolution InstaGLO R180 Toaster', 'Revolution InstaGLO R180 Toaster. 2-Slice, high-end stainless steel design. Features touchscreen with high-speed smart settings for perfectly toasted bagels, English muffins, toast, Pop-Tarts and waffles. Fresh, frozen, or reheat with 7 toastiness levels.', 'Kitchen'); home aply
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (8,'D-Link Battery', 'D-Link Battery Powered Wi-Fi Water/Flood Sensor - 90 dB Siren - no hub Required - mydlink app - Push Notifications - Mobile Phone Alerts - (DCH-S161/2PK RE) 2 Pack Certified Open Box', 'gadgets');
+VALUES (8,'D-Link Battery', 'D-Link Battery Powered Wi-Fi Water/Flood Sensor - 90 dB Siren - no hub Required - mydlink app - Push Notifications - Mobile Phone Alerts - (DCH-S161/2PK RE) 2 Pack Certified Open Box', 'gadgets'); gadgets 
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (9,'Brilliant Smart Home Control (2-Switch Panel)', 'Brilliant Smart Home Control (2-Switch Panel) — Alexa Built-In & Compatible with Ring, Sonos, Hue, Google Nest, Wemo, SmartThings, Apple HomeKit — In-Wall Touchscreen Control for Lights, Music, & More', 'smart');
+VALUES (9,'Brilliant Smart Home Control (2-Switch Panel)', 'Brilliant Smart Home Control (2-Switch Panel) — Alexa Built-In & Compatible with Ring, Sonos, Hue, Google Nest, Wemo, SmartThings, Apple HomeKit — In-Wall Touchscreen Control for Lights, Music, & More', 'smart'); home aply
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (10,'Fire TV ', 'Fire TV Stick 4K streaming device with Alexa Voice Remote (includes TV controls), Dolby Vision', 'gadgets');
+VALUES (10,'Fire TV ', 'Fire TV Stick 4K streaming device with Alexa Voice Remote (includes TV controls), Dolby Vision', 'gadgets'); gadgets
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (11,'Light Bulb Security Camera', 'Light Bulb Security Camera, E27 Wireless WiFi Outdoor Security Camera, 1080P 2.4GHz WiFi Smart Home Surveillance Cam 355 Degree with Two-Way Audio, Night Vision, Motion Detection, Work with Alexa', 'security');
+VALUES (11,'Light Bulb Security Camera', 'Light Bulb Security Camera, E27 Wireless WiFi Outdoor Security Camera, 1080P 2.4GHz WiFi Smart Home Surveillance Cam 355 Degree with Two-Way Audio, Night Vision, Motion Detection, Work with Alexa', 'security'); security
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (12,'Aqara Wireless Mini Switch', 'Aqara Wireless Mini Switch, Requires AQARA HUB, Zigbee Connection, Versatile 3-Way Control Button for Smart Home Devices, Compatible with Apple HomeKit', 'Eletrical');
+VALUES (12,'Aqara Wireless Mini Switch', 'Aqara Wireless Mini Switch, Requires AQARA HUB, Zigbee Connection, Versatile 3-Way Control Button for Smart Home Devices, Compatible with Apple HomeKit', 'Eletrical'); Eletrical
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (13,'Smart Home Energy Monitor with 16 50A', 'Smart Home Energy Monitor with 16 50A Circuit Level Sensors | Vue - Real Time Electricity Monitor/Meter | Solar/Net Metering', 'Eletrical');
+VALUES (13,'Smart Home Energy Monitor with 16 50A', 'Smart Home Energy Monitor with 16 50A Circuit Level Sensors | Vue - Real Time Electricity Monitor/Meter | Solar/Net Metering', 'Eletrical'); Eletrical
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (14,'Intelligent Wake Up Light', 'Intelligent Wake Up Light, Sunrise Alarm Clock Supported Alexa/Google Home, Clock Radio, Sunrise Simulation & Sleep Aid, 4 Alarms with FM Radio/7 Natural Sounds/Snooze, 7 Colored Night Light for Adults, Kids', 'smart');
+VALUES (14,'Intelligent Wake Up Light', 'Intelligent Wake Up Light, Sunrise Alarm Clock Supported Alexa/Google Home, Clock Radio, Sunrise Simulation & Sleep Aid, 4 Alarms with FM Radio/7 Natural Sounds/Snooze, 7 Colored Night Light for Adults, Kids', 'smart'); home aply
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (15,'SONOFF NSPanel-US WiFi Wall Switch', 'SONOFF NSPanel-US WiFi Wall Switch (2 Groups), Wireless Switch, Wireless Home Automation Thermostat, Application Control, Smart Scene, Compatible for Alexa/Google Home', 'smart');
+VALUES (15,'SONOFF NSPanel-US WiFi Wall Switch', 'SONOFF NSPanel-US WiFi Wall Switch (2 Groups), Wireless Switch, Wireless Home Automation Thermostat, Application Control, Smart Scene, Compatible for Alexa/Google Home', 'smart'); climatization
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (16,'Amazon Smart Air Quality Monitor', 'Introducing Amazon Smart Air Quality Monitor – Know your air, Works with Alexa– A Certified for Humans Device', 'welness');
+VALUES (16,'Amazon Smart Air Quality Monitor', 'Introducing Amazon Smart Air Quality Monitor – Know your air, Works with Alexa– A Certified for Humans Device', 'welness'); welness
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (17,'Smart Lock, Hornbill', 'Smart Lock, Hornbill Fingerprint Keyless Entry Door Lock with Keypad, Digital Smart Locks for Entry Front Door, Smart Deadbolt for Wooden Door, Unlock by Bluetooth, ekey, FOB, Passcodes, Auto Lock,APP', 'security');
+VALUES (17,'Smart Lock, Hornbill', 'Smart Lock, Hornbill Fingerprint Keyless Entry Door Lock with Keypad, Digital Smart Locks for Entry Front Door, Smart Deadbolt for Wooden Door, Unlock by Bluetooth, ekey, FOB, Passcodes, Auto Lock,APP', 'security'); security
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (18,'Robot Vacuums and Mop Combo OSOTEK', 'Robot Vacuums and Mop Combo OSOTEK, 3500Pa Suction WiFi/App/Alexa Control, 150mins Runtime, Self-Charging Robotic Vacuum Cleaner, Smart Gyroscope Navigation, Ideal for Hard Floors Pet Hair Carpets', 'cleaning');
+VALUES (18,'Robot Vacuums and Mop Combo OSOTEK', 'Robot Vacuums and Mop Combo OSOTEK, 3500Pa Suction WiFi/App/Alexa Control, 150mins Runtime, Self-Charging Robotic Vacuum Cleaner, Smart Gyroscope Navigation, Ideal for Hard Floors Pet Hair Carpets', 'cleaning'); cleaning
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (19,'Dreo Tower Fan', 'Dreo Tower Fan, Fan, 90° Oscillating 4 Modes, Fan with Remote, Large LED Display, Quiet Fan, Touch Control, Powerful Floor Fan, Timer, Bladeless Standing Fan for Bedroom', 'climatization');
+VALUES (19,'Dreo Tower Fan', 'Dreo Tower Fan, Fan, 90° Oscillating 4 Modes, Fan with Remote, Large LED Display, Quiet Fan, Touch Control, Powerful Floor Fan, Timer, Bladeless Standing Fan for Bedroom', 'climatization'); climatization
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (20,'TKSTAR Magnetic GPS Tracker for Car', 'TKSTAR Magnetic GPS Tracker for Car, 5 Months Long Standby GPS Tracking, Waterproof Real Time Tracking GPS Locator Professional Anti-Lost GPS Alarm Tracker for Car Truck Moto Boat TK905B', 'tracking');
+VALUES (20,'TKSTAR Magnetic GPS Tracker for Car', 'TKSTAR Magnetic GPS Tracker for Car, 5 Months Long Standby GPS Tracking, Waterproof Real Time Tracking GPS Locator Professional Anti-Lost GPS Alarm Tracker for Car Truck Moto Boat TK905B', 'tracking'); tracking
 
 INSERT INTO `term_project`.`product`    (`product_id`, `title`, `description`, `type`) 
-VALUES (21,'Amazon Fire TV 50"', 'Amazon Fire TV 50" Omni Series 4K UHD smart TV, hands-free with Alexa', 'TV');
+VALUES (21,'Amazon Fire TV 50"', 'Amazon Fire TV 50" Omni Series 4K UHD smart TV, hands-free with Alexa', 'TV'); gadgets
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- categury Table
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (1, 1, 'Home climatization');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (2, 2, 'Home apliance');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (3,3, 'Eletrical');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (4,4, 'Home decoration');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (5,5, 'gadgets');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (6,6, 'Home security');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (7,7, 'wellness');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (8,8, 'home cleaning');
+
+INSERT INTO `term_project`.`category` (`category_id`, `parent_id`, `title`) 
+VALUES (9,9, 'tracking');
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- product_category  Table
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(1,1);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(2,2);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(3,1);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(4,1);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(5,3);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(6,4);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(7,2);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(8,5);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(9,2);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(10,5);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(11,6);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(12,3);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(13,3);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(14,2);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(15,1);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(16,7);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(17,6);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(18,8);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(19,1);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(20,9);
+iNSERT INTO term_project.product_category(product_id,category_id) 
+VALUES(21,5);
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Costumer   Table
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (2, 'Parth', 'Thakkar', '23 Fort Dear', 'parth@gmail.com', '9057824106');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (2, 'Dapen', 'Panoi', '11 Fort Dear', 'Dapen@gmail.com', '9171895049');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (3, 'Xyubi', 'Nuaxi', '23 North land', 'Nuaxi@gmail.com', '2230507118');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (4, 'Xakuo', 'Tiexol', '23 under the bidge north', 'Tiexol@gmail.com', '1455849193');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (5, 'Tiexol', 'Pebal', '457 haven greek', 'Pebal@gmail.com', '3843702926');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (6, 'Arnui', 'Kionr', '4848 so far away ', 'Kionr@gmail.com', '507413015');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (7, 'Foeko', 'Kehuequo', '145 no house west', 'Kehuequo@gmail.com', '4883862091');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (8, 'Parth', 'Thakkar', '23 Fort Dear', 'parth@gmail.com', '9664593995');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (9, 'Pebal', 'Daipa', '587 Valhala north avenue', 'Daipa@gmail.com', '5552019400');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (10, 'Ustol', 'Sies', '865 God help me to pass north west street', 'Sies@gmail.com', '8589098167');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (11, 'Ustol', 'Baba', '4876 my creativity has gone here road', 'Baba@gmail.com', '5787961540');
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Costumer   Table
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (2, 'Parth', 'Thakkar', '23 Fort Dear', 'parth@gmail.com', '9057824106');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (2, 'Dapen', 'Panoi', '11 Fort Dear', 'Dapen@gmail.com', '9171895049');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (3, 'Xyubi', 'Nuaxi', '23 North land', 'Nuaxi@gmail.com', '2230507118');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (4, 'Xakuo', 'Tiexol', '23 under the bidge north', 'Tiexol@gmail.com', '1455849193');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (5, 'Tiexol', 'Pebal', '457 haven greek', 'Pebal@gmail.com', '3843702926');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (6, 'Arnui', 'Kionr', '4848 so far away ', 'Kionr@gmail.com', '507413015');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (7, 'Foeko', 'Kehuequo', '145 no house west', 'Kehuequo@gmail.com', '4883862091');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (8, 'Parth', 'Thakkar', '23 Fort Dear', 'parth@gmail.com', '9664593995');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (9, 'Pebal', 'Daipa', '587 Valhala north avenue', 'Daipa@gmail.com', '5552019400');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (10, 'Ustol', 'Sies', '865 God help me to pass north west street', 'Sies@gmail.com', '8589098167');
+
+INSERT INTO `term_project`.`costumer` (`customer_id`, `first_name`, `last_name`, `address`, `email`, `phone`) 
+VALUES (11, 'Ustol', 'Baba', '4876 my creativity has gone here road', 'Baba@gmail.com', '5787961540');
+
